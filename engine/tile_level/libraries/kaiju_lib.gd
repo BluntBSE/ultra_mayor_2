acecc02fb4dcce9d24cb39bfc04e1846d6632421
Robert Meyer <ram.news1@gmail.com>
2024-12-13 20:35:29 -0500

    Atonement

--- engine/tile_level/libraries/kaiju_lib.gd
+++ engine/tile_level/libraries/kaiju_lib.gd
@@ -1,6 +1,13 @@
 class_name KaijuLib
 
 #TODO: Make portrait and sprite fetchers pull from a global path
+
+static func fetch_art_pack(key:String)->Resource:
+	var dir:String = "res://engine/tile_level/kaiju/art_packs/"
+	var path:String = dir+key+".tres"
+	print("FETCH ART PACK IS LOADING FROM ", path)
+	return load(path)
+	
 static var lib: Dictionary = {
 	"raiju":
 	(
@@ -9,7 +16,7 @@ static var lib: Dictionary = {
 			{
 				"id": "raiju",
 				"display_name": "Raiju",
-				"sprite": "C:/Users/ramne/OneDrive/Desktop/ultra_mayor/ultra_mayor_2/engine/tile_level/kaiju/assets/sprites/f2_tank.png",
+				"sprite": "res://engine/tile_level/kaiju/assets/sprites/f2_tank.png",
 				"portrait": "res://engine/tile_level/kaiju/assets/portraits/magmar_youngsilithar.jpg",
 				"move_points": 4,
 				"moves_remaining": 4,
@@ -17,42 +24,10 @@ static var lib: Dictionary = {
 				"limbs": ["head", "claws", "claws", "tail"],
 				"types": ["physical", "electric"],
 				"health_factor": 0.75,
-				"art_pack": ArtPacks.lib["raiju"]
+				"art_pack": "raiju"
 			}
 		)
 	),
-	"dragon":
-	LogicalKaiju.new(
-		{
-			"id": "dragon",
-			"display_name": "Dragon",
-			"sprite": "res://engine/tile_level/kaiju/assets/sprites/AI_dragon_sprite.png",
-			"portrait": "res://engine/tile_level/kaiju/assets/sprites/AI_blue_dragon_portrait.jpg",
-			"move_points": 3,
-			"moves_remaining": 3,
-			"speed_chart": SpeedChart.new({"mountain": 1, "water": 1}),
-			"limbs": ["head", "wings", "claws", "legs", "tail"],
-			"health_factor": 1.25,
-			"types": ["physical", "fire"],
-			"art_pack": ArtPacks.lib["dragon"]
-		}
-	),
-	"bird":
-	LogicalKaiju.new(
-		{
-			"id": "bird",
-			"display_name": "Dactyl",
-			"sprite": "res://engine/tile_level/kaiju/assets/sprites/AI_bird_sprite.png",
-			"portrait": "res://engine/tile_level/kaiju/assets/sprites/AI_bird_sprite.png",
-			"move_points": 4,
-			"moves_remaining": 4,
-			"speed_chart": SpeedChart.new({"mountain": 1, "water": 1}),
-			"limbs": ["head", "wings", "legs"],
-			"health_factor": 1.0,
-			"types": ["physical"],
-			"art_pack": ArtPacks.lib["bird"]
-		}
-	)
 }
 #Tech tree improvements go in _init as an instructor callable...
 #When we get there.
